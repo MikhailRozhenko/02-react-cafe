6f53158758bdb087a4917c1357fa831f2cd8b1a6
Mykhailo Rozhenko <mihailkfmn@gmail.com>
2026-03-22 22:19:00 +0200

    fix base for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,7 @@ import babel from '@rolldown/plugin-babel';
 import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/02-react-cafe/',
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
   build: {
     sourcemap: true,
